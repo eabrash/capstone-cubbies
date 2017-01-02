d557a6df1ec2612b6bf0fde39d56fffaa6f6881f
Emily Abrash <emabrash@gmail.com>
2017-01-02 13:13:14 -0800

    Initial Commit

---
+++ opengl-xcode-test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opengl-xcode-test
+//
+//  Created by Emily Abrash on 1/2/17.
+//  Copyright © 2017 Emily Abrash. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
